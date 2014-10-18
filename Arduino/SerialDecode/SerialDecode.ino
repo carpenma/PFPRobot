6c8e86310aaee151dfca13d2ff5237aba4dd6c24
Matt <carpenma@gmail.com>
2014-10-18 13:05:44 -0400

    Created Arduino file for translating serial comms into control of robot systems.

---
+++ Arduino/SerialDecode/SerialDecode.ino
@@ -0,0 +1,15 @@
+// Matthew Carpenter - carpent6@purdue.edu
+// This program is designed to recieve sets of commands over serial, parse them then change the state of various outputs accordingly
+
+/* Mapping of first argument to board output:
+  0 - HALT ALL PWM
+  
+  */
+
+void setup () {
+  
+}
+
+void loop () {
+  
+}
