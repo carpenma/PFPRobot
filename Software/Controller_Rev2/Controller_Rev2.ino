be7b479607120ba6d0fbfe30d51046cc9faf7146
Matt <carpenma@gmail.com>
2015-06-04 23:59:42 -0400

    Changing modes works
    Added vibration when transitioning enabled <-> disabled
    Controller LEDs also properly signify mode now:
    Spinning = Disabled
    Upper left on = Enabled

--- Software/Controller_Rev2/Controller_Rev2.ino
+++ Software/Controller_Rev2/Controller_Rev2.ino
@@ -2,6 +2,9 @@
 #include <SPI.h>
 #include <XBOXRECV.h>
 
+#define FALSE 0
+#define TRUE 1
+
 USB Usb;  //Create a USB instance to connect to the wireless dongle
 XBOXRECV Xbox(&Usb); //Create wireless XBOX360 controller instance on the USB port
 
@@ -10,17 +13,18 @@ Servo leftSide;
 Servo rightSide;
 
 boolean isEnabled;
+boolean last_connected;
 const int LEFT_PWM = 9;
 const int RIGHT_PWM = 10;
 const int DEADZONE_L = 0;
 const int DEADZONE_R = 0;
-const unsigned long TIMEOUT = 250;  // Time allowed between commands (in ms) before the robot automatically disables
+const unsigned long TIMEOUT = 500;  // Time allowed between commands (in ms) before the robot automatically disables
 
 int leftStick_Y;
 int rightStick_Y;
 int leftSpeed;
 int rightSpeed;
-unsigned long lastData = 250;
+unsigned long lastData;
 
 void setup() {
   Serial.begin(9600);
@@ -31,51 +35,77 @@ void setup() {
     while (1);
   }
   Serial.println("Wireless receiver library loaded successfuly!");
-
+  delay(250);
   int leftSpeed = 90;
   int rightSpeed = 90;
+
+  lastData = millis();
 }
 
 void loop() {
   Usb.Task();
   if (Xbox.XboxReceiverConnected) {
     if (Xbox.Xbox360Connected[0]) {
-      if(isEnabled & millis() - lastData <= TIMEOUT) {
+      if(!last_connected) Serial.println("Controller Connected!");
+      //Serial.print("isEnabled: "); Serial.println(isEnabled);
+      //delay(50);
+      //Serial.println(millis() - lastData);
+      if(isEnabled && millis() - lastData <= TIMEOUT && millis() > 500) {
+        Serial.println("Running Robot!");
         runRobot();
       }
-      if(Xbox.getButtonClick(START, 0) {
+      else {
+        disable();
+      }
+      if(Xbox.getButtonClick(START, 0)) {
+        //Serial.println("Start Button Detected!");
         if(isEnabled) disable();
         else enable();
       }
     }
     else {
+      Serial.println("Error: No Controller!");
       disable(); // Lost contact with controller
       delay(500);
     }
   }
   else {
+    Serial.println("Error: No Receiver!");
     disable();  // Lost contact with receiver
     delay(1000);
   }
+  last_connected = Xbox.Xbox360Connected[0];
 }
 
 void enable() {
-  isEnabled = TRUE;
+  //Serial.print("Enabling! - "); Serial.println(isEnabled);
   leftSide.attach(LEFT_PWM);
   rightSide.attach(RIGHT_PWM);
 
-  //Change status of controller LEDs if status just changed
-  //Vibrate?
+  if(!isEnabled && Xbox.XboxReceiverConnected && Xbox.Xbox360Connected[0]) {
+    //Serial.println("Enabled Transition!");
+    Xbox.setLedOn(LED1,0);  //May change this so that different LEDs come on for different speeds/modes etc.
+    Xbox.setRumbleOn(60000,60000,0);
+    delay(200);
+    Xbox.setRumbleOff(0);
+  }
+  isEnabled = TRUE;
 }
 
 void disable() {
+  //Serial.print("Disabling! - "); Serial.println(isEnabled);
+  leftSide.detach();
+  rightSide.detach();
+
+  if(isEnabled && Xbox.XboxReceiverConnected && Xbox.Xbox360Connected[0]) {
+    //Serial.println("Disabled Transition!");
+    Xbox.setLedMode(ALTERNATING, 0);
+    Xbox.setRumbleOn(60000,60000,0);
+    delay(200);
+    Xbox.setRumbleOff(0);
+  }
   isEnabled = FALSE;
-  leftSide.detach(LEFT_PWM);
-  rightSide.detach(RIGHT_PWM);
-
-  //Change status of the controller LEDs if robot was just disabled
-  // Vibrate?
-
+  lastData = millis();
 }
 
 void runRobot() {
@@ -89,7 +119,12 @@ void moveRobot() {
   leftSpeed = map(leftStick_Y,-32767,32768,0,180);
   rightSpeed = map(rightStick_Y,-32767,32768,0,180);
 
-  //Deadzone calc
+  if (leftSpeed <= (90 + DEADZONE_L) & leftSpeed >= (90 - DEADZONE_L)) {
+    leftSpeed = 90;
+  }
+  if(rightSpeed <= (90 + DEADZONE_R) & rightSpeed >= (90 - DEADZONE_R)) {
+    rightSpeed = 90;
+  }
 
   leftSide.write(leftSpeed);
   rightSide.write(rightSpeed);
